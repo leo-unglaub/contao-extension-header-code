164c691328c280a63ce8328c45ea0a48d5a7a42a
LeoUnglaub <leo@leo-unglaub.net>
2011-01-15 22:14:08 +0000

    - fix a broken language file (thanks to Michel LAURENT)
    git-svn-id: http://svn.lu-hosting.net/header_code@8 c7baa9ea-afc5-47ac-a193-5719a4c1e291

--- trunk/system/modules/header_code/languages/en/tl_page.php
+++ trunk/system/modules/header_code/languages/en/tl_page.php
@@ -29,7 +29,7 @@
  */
 
 $GLOBALS['TL_LANG']['tl_page']['hc_code']			= array('individual header code', 'Place here any header code. This may be META data, .js or .css code.');
-$GLOBALS['TL_LANG']['tl_page']['hc_footer_code']	= array('individual footer code', 'Place here any footer code.  The footer code is placed before the closing </div> from #wrapper.');
+$GLOBALS['TL_LANG']['tl_page']['hc_footer_code']	= array('individual footer code', 'Place here any footer code.  The footer code is placed before the closing div container from #wrapper.');
 $GLOBALS['TL_LANG']['tl_page']['hc_inheritance']	= array('inherit code', 'Select Yes if you want to inheritance the header/footer code to every subpage. Select No to show this code only in the actual page.');
 $GLOBALS['TL_LANG']['tl_page']['hc_mode']			= array('Select the display mode', 'Here you can select if you want to add the code from the actual site only or every code element witch is marked as inheritance.');
 
