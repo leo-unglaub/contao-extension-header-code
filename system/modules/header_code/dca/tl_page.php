55cff81744f15a5c4ce70bbdaccc1ce9a56d9bcb
Leo Unglaub <leo.unglaub@2create.at>
2013-01-14 16:57:46 +0100

    Reformat the file
    With this commit i am reformating the tl_page dca
    file. Now it's better readable in a terminal editor.
    
    Signed-off-by: Leo Unglaub <leo.unglaub@2create.at>

--- system/modules/header_code/dca/tl_page.php
+++ system/modules/header_code/dca/tl_page.php
@@ -31,8 +31,19 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_page']['palettes']['regular']	= str_replace('{layout_legend:hide}', '{header_code_legend},hc_code,hc_footer_code,hc_inheritance,hc_mode;{layout_legend:hide}', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root']		= str_replace('{dns_legend}', '{header_code_legend},hc_code,hc_footer_code,hc_inheritance,hc_mode;{dns_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace
+(
+	'{layout_legend:hide}',
+	'{header_code_legend},hc_code,hc_footer_code,hc_inheritance,hc_mode;{layout_legend:hide}',
+	$GLOBALS['TL_DCA']['tl_page']['palettes']['regular']
+);
+
+$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace
+(
+	'{dns_legend}',
+	'{header_code_legend},hc_code,hc_footer_code,hc_inheritance,hc_mode;{dns_legend}',
+	$GLOBALS['TL_DCA']['tl_page']['palettes']['root']
+);
 
 
 /**
@@ -43,7 +54,14 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['hc_code'] = array
 	'label'			=> &$GLOBALS['TL_LANG']['tl_page']['hc_code'],
 	'exclude'		=> true,
 	'inputType'		=> 'textarea',
-	'eval'			=> array('tl_class'=>'long clr', 'preserveTags'=>true, 'decodeEntities'=>false, 'allowHtml'=>true, 'style'=>'height:150px')
+	'eval'			=> array
+	(
+		'tl_class'			=> 'long clr',
+		'preserveTags'		=> true,
+		'decodeEntities'	=> false,
+		'allowHtml'			=> true,
+		'style'				=> 'height:150px',
+	)
 );
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['hc_footer_code'] = array
@@ -51,7 +69,14 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['hc_footer_code'] = array
 	'label'			=> &$GLOBALS['TL_LANG']['tl_page']['hc_footer_code'],
 	'exclude'		=> true,
 	'inputType'		=> 'textarea',
-	'eval'			=> array('tl_class'=>'long clr', 'preserveTags'=>true, 'decodeEntities'=>false, 'allowHtml'=>true, 'style'=>'height:150px')
+	'eval'			=> array
+	(
+		'tl_class'			=> 'long clr',
+		'preserveTags'		=> true,
+		'decodeEntities'	=> false,
+		'allowHtml'			=> true,
+		'style'				=> 'height:150px',
+	)
 );
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['hc_inheritance'] = array
@@ -60,8 +85,15 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['hc_inheritance'] = array
 	'exclude'		=> true,
 	'default'		=> 1,
 	'inputType'		=> 'select',
-	'options'  		=> array(1=>$GLOBALS['TL_LANG']['MSC']['yes'], 0=>$GLOBALS['TL_LANG']['MSC']['no']),
-	'eval'			=> array('tl_class'=>'w50')
+	'options'  		=> array
+	(
+		1 => $GLOBALS['TL_LANG']['MSC']['yes'],
+		0 => $GLOBALS['TL_LANG']['MSC']['no'],
+	),
+	'eval'			=> array
+	(
+		'tl_class' => 'w50',
+	)
 );
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['hc_mode'] = array
@@ -70,8 +102,15 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['hc_mode'] = array
 	'exclude'		=> true,
 	'default'		=> 1,
 	'inputType'		=> 'select',
-	'options'		=> array(1=>$GLOBALS['TL_LANG']['tl_page']['hc_mode_add'], 0=>$GLOBALS['TL_LANG']['tl_page']['hc_mode_override']),
-	'eval'			=> array('tl_class'=>'w50')
+	'options'		=> array
+	(
+		1 => $GLOBALS['TL_LANG']['tl_page']['hc_mode_add'],
+		0 => $GLOBALS['TL_LANG']['tl_page']['hc_mode_override'],
+	),
+	'eval'			=> array
+	(
+		'tl_class' => 'w50',
+	)
 );
 
 
