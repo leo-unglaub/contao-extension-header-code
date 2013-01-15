e2ff234ce29647155db9e64249886d9d2acb81d6
Leo Unglaub <leo.unglaub@2create.at>
2013-01-15 03:41:04 +0100

    Fixing the bug #3
    With this commit i am trying to fix the bug #3. The bug
    causes the frontend to die with an exception if the user
    does the database update not fast enough.
    
    Also i am changing the linebreaks to to the default PHP
    constant. Now windows servers also return the right
    linebreak.
    
    Also i am fixng some typos in the the phpdoc comments.
    
    Signed-off-by: Leo Unglaub <leo.unglaub@2create.at>

--- system/modules/header_code/HeaderCode.php
+++ system/modules/header_code/HeaderCode.php
@@ -32,14 +32,14 @@
  * Class HeaderCode
  * Contain methods to handle the the header code
  */
-class HeaderCode extends Frontend
+class HeaderCode extends Controller
 {
 	/**
 	 * Add the Header Code to the Site (Hook landing)
 	 *
-	 * @param string $strContent
-	 * @param string $strTemplate
-	 * @return string
+	 * @param	string	$strContent		The content of the template.
+	 * @param	string	$strTemplate	The name of the template.
+	 * @return	string					The content of the template.
 	 */
 	public function addHeaderCode($strContent, $strTemplate)
 	{
@@ -58,29 +58,34 @@ class HeaderCode extends Frontend
 	/**
 	 * crawl the page tree to find parrent entry's
 	 *
-	 * @param int $intId
-	 * @return void
+	 * @param	int		$intId	The if of the page that should be parsed.
+	 * @return	void			This method returns nothing.
 	 */
 	private function crawlTlPage($intId)
 	{
+		// import some libs
+		// DON'T remove this import, the Controller class will fail
+		// without this import
 		$this->import('Database');
+
+		// define some variables
 		$intOldId = $intId;
 		$strBufferHead = '';
 		$strBufferFoot = '';
 
+
+		// walk thru every page until we hit 0
 		while ($intId > 0)
 		{
-			$objRow = $this->Database->prepare('SELECT id,pid,hc_code,hc_footer_code,hc_inheritance,hc_mode FROM tl_page WHERE id=?')
-									 ->limit(1)
-									 ->execute($intId);
+			$objRow = $this->getPageDetails($intId);
 
 			// if the actual page has header code
 			if ((strlen($objRow->hc_code) || strlen($objRow->hc_footer_code)) && $intOldId == $objRow->id)
 			{
 				if ($objRow->hc_mode == 1)
 				{
-					$strBufferHead .= "\n" . $objRow->hc_code;
-					$strBufferFoot .= "\n" . $objRow->hc_footer_code;
+					$strBufferHead .= PHP_EOL . $objRow->hc_code;
+					$strBufferFoot .= PHP_EOL . $objRow->hc_footer_code;
 				}
 				else
 				{
@@ -92,17 +97,17 @@ class HeaderCode extends Frontend
 				}
 			}
 
-			// check the parrents
+			// check the parents
 			if ((strlen($objRow->hc_code) || strlen($objRow->hc_footer_code)) && $intOldId !== $objRow->id && $objRow->hc_inheritance == 1)
 			{
 				if (count($objRow->hc_code))
 				{
-					$strBufferHead .= "\n" . $objRow->hc_code;
+					$strBufferHead .= PHP_EOL . $objRow->hc_code;
 				}
 
 				if (count($objRow->hc_footer_code))
 				{
-					$strBufferFoot .= "\n" . $objRow->hc_footer_code;
+					$strBufferFoot .= PHP_EOL . $objRow->hc_footer_code;
 				}
 			}
 
@@ -110,9 +115,11 @@ class HeaderCode extends Frontend
 			$intId = $objRow->pid;
 		}
 
+
 		// add the code to the right channel
 		$GLOBALS['TL_HEAD'][] = $strBufferHead;
 		$GLOBALS['TL_MOOTOOLS'][] = $strBufferFoot;
+
 
 		// after the first run the code is in the header so we can skip all other templates
 		$GLOBALS['header_code_stop'] = true;
